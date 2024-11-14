a587e5c885acba373b2a82c1105c9d15390a1522
joaquinyjoa <joaquinalfredogreco@gmail.com>
2024-11-14 01:58:08 +0000

    Agregue el montaje de sudo mkfs.ext4 /dev/mapper/vg_datos-lv_workareas

--- RTA_Examen_20241114/Punto_A.sh
+++ RTA_Examen_20241114/Punto_A.sh
@@ -9,3 +9,4 @@ sudo vgcreate vg_temp /dev/sde2
 sudo lvcreate -L +5MB vg_datos -n lv_docker
 sudo lvcreate -l +100%FREE vg_datos -n lv_workareas
 sudo mkfs.ext4 /dev/mapper/vg_datos-lv_docker
+sudo mkfs.ext4 /dev/mapper/vg_datos-lv_workareas
