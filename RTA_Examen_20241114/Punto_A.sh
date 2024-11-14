b8100c940fe7479d2899d2aa4fd51d6a12899790
joaquinyjoa <joaquinalfredogreco@gmail.com>
2024-11-14 01:56:30 +0000

    Formate el /dev/mapper/vg_datos-lv_docker

--- RTA_Examen_20241114/Punto_A.sh
+++ RTA_Examen_20241114/Punto_A.sh
@@ -8,3 +8,4 @@ sudo pvcreate /dev/sde2
 sudo vgcreate vg_temp /dev/sde2
 sudo lvcreate -L +5MB vg_datos -n lv_docker
 sudo lvcreate -l +100%FREE vg_datos -n lv_workareas
+sudo mkfs.ext4 /dev/mapper/vg_datos-lv_docker
