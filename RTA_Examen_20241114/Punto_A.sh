10ccf6edfcc7f1266b4d1d45390b4350b15f82e4
joaquinyjoa <joaquinalfredogreco@gmail.com>
2024-11-14 02:13:41 +0000

    Agregue el montaje de workareas

--- RTA_Examen_20241114/Punto_A.sh
+++ RTA_Examen_20241114/Punto_A.sh
@@ -11,3 +11,4 @@ sudo lvcreate -l +100%FREE vg_datos -n lv_workareas
 sudo mkfs.ext4 /dev/mapper/vg_datos-lv_docker
 sudo mkfs.ext4 /dev/mapper/vg_datos-lv_workareas
 sudo mount /dev/mapper/vg_datos-lv_docker /var/lib/docker/
+sudo mount /dev/mapper/vg_datos-lv_workareas /work/
